ffc1f671ded050455e8e296f66eb25a4443e347e
Chantale <shantelenos99@gmail.com>
2024-07-02 00:12:31 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -9,19 +8,34 @@ from models import db, Earthquake
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
-    return make_response(body, 200)
+    return make_response(jsonify(body), 200)
 
-# Add views here
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    quake_list = []
+    for earthquake in earthquakes:
+        quake_list.append({
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        })
 
+    response = {
+        "count": len(quake_list),
+        "quakes": quake_list
+    }
+    
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
